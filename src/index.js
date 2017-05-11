fb5caf1dca3914b326533f0fe6589e6a52226dfc
Sergey Verevkin <sergey.veryovkin@swanteams.com>
2017-05-11 09:59:39 +0300

    Setup Apollo client and provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 import App from './App';
 import './index.css';
 
+// Setup Apollo client network connection
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj2j530bqbrr20175k1dd39eg'}),
+});
+
 ReactDOM.render((
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   ),
   document.getElementById('root')
 );
