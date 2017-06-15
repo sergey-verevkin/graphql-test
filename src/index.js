28daaafdd73a6dd79f0ab056e266a3c1ddd810dc
Sergey Verevkin <sergey.veryovkin@swanteams.com>
2017-06-15 10:35:19 +0300

    Redux error middleware

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ const userReducer = (state = {}, action) => {
 
       return { ...state, age: action.payload }
     }
+    case 'THROW_ERROR': {
+      throw new Error('AAAAHHHH!')
+    }
     default: {
       return state
     }
@@ -45,7 +48,15 @@ const logger = (store) => (next) => (action) => {
   next(action);
 };
 
-const middleware = applyMiddleware(logger);
+const error = (store) => (next) => (action) => {
+  try {
+    next(action);
+  } catch (e) {
+    console.log('Error happened!', e);
+  }
+};
+
+const middleware = applyMiddleware(logger, error);
 
 const store = createStore(reducers, middleware);
 
@@ -54,5 +65,6 @@ store.subscribe(() => {
 });
 
 store.dispatch({ type: 'CHANGE_NAME', payload: 'Verik' });
+store.dispatch({ type: 'THROW_ERROR' });
 store.dispatch({ type: 'CHANGE_AGE', payload: '32' });
 store.dispatch({ type: 'ADD_MESSAGE', payload: 'message' });
