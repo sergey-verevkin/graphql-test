42aa6ce90f721e4c025fd11674ba2dea4073dbda
Sergey Verevkin <sergey.veryovkin@swanteams.com>
2017-06-15 10:12:07 +0300

    Initial Redux example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore } from 'redux'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INC': {
+      return state + action.payload;
+    }
+    case 'DEC': {
+      return state - action.payload;
+    }
+    default: {
+      return state
+    }
+  }
+};
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("Store changed ", store.getState())
+});
+
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+store.dispatch({ type: 'DEC', payload: 1 });
+store.dispatch({ type: 'DEC', payload: 1 });
+store.dispatch({ type: 'INC', payload: 1 });
+
